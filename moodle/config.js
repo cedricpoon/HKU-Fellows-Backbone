e7bd70e2c14b5341e6630da27889a82361cbae6d
Cedric Poon <cedric@poons.me>
2018-12-31 03:41:26 +0800

    finish crawling all posts from course

---
+++ moodle/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // delay for faking Moodle server we are humans
+  delay: 100,
+};
--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,31 @@ router.route('/database').get(async (req, res) => {
   }
 });
 
+// -----------------------------------------------
+//  Example of crawling all posts from a course
+// -----------------------------------------------
+// router.route('/test/:idx').post((req, res) => {
+//   let courses = [];
+//   let posts = [];
+//   const { idx } = req.params;
+//   const cookieString = decrypt(req.body.moodleKey);
+//
+//   crawler.getCourses({ cookieString })
+//     .then((_courses) => {
+//       courses = _courses;
+//     })
+//     .then(async () => {
+//       posts = await crawler.retrievePostsFromCourse({
+//         cookieString,
+//         coursePath: courses[idx].path,
+//       });
+//     })
+//     .then(() => {
+//       res.json(posts);
+//     })
+//     .catch((e) => {
+//       res.json(e);
+//     });
+// });
+
 module.exports = router;
