2a12ab10c18738d8190921f05de0d315921e3b75
Cedric Poon <cedric@poons.me>
2019-01-05 20:04:51 +0800

    Feature/no native cache (#18)
    * use offset to govern sliding window
    
    * Update openapi.yml

--- database/config.js
+++ database/config.js
@@ -4,11 +4,10 @@ const configure = database => ({
   password: process.env.RDS_PASSWORD,
   port: 3306,
   database,
-  connectionLimit: 66 / 2, // t2.micro max = 66
+  connectionLimit: 66, // t2.micro max = 66
 });
 
 module.exports = {
   config: configure,
   database: 'hkufdb',
-  cacheDatabase: 'hkufdb_cache',
 };
