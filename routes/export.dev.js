67dce2562a20c00edda84c15bb947fe9c6133b3a
Cedric Poon <cedric@poons.me>
2019-01-07 15:49:17 +0800

    single caching for moodle (#24)
    minor update

--- routes/export.dev.js
+++ routes/export.dev.js
@@ -7,88 +7,79 @@ const { db } = require('../database/connect');
 
 const router = express.Router();
 
+/* eslint-disable no-restricted-syntax, no-await-in-loop */
+const deepLinkToRight = async (left, rightObj, { idName, childrenQuery }) => {
+  const leftObj = {
+    id: left[idName],
+    title: left.Description,
+    links: [],
+    children: [],
+  };
+
+  // linking all children
+  const children = await db.query(childrenQuery);
+  for (const child of children) {
+    await deepLinkToRight(child, leftObj, {
+      idName: 'DepartmentId',
+      childrenQuery: `
+        select * from Department
+          where SchoolId = "${left.DepartmentId}"
+      `,
+    });
+  }
+
+  rightObj.children.push(leftObj); // linking
+
+  const courses = await db.query(`
+    select * from Course
+      where ${idName} = "${left[idName]}"
+  `);
+
+  for (const course of courses) {
+    // linking
+    leftObj.links.push({
+      id: course.CourseId,
+      title: course.CourseId.toUpperCase(),
+      description: course.Description,
+    });
+  }
+  // unset empty links
+  if (leftObj.links.length === 0) {
+    delete leftObj.links;
+  } else {
+    // sort links
+    leftObj.links.sort((a, b) => a.id - b.id);
+  }
+  // unset empty children
+  if (leftObj.children.length === 0) {
+    delete leftObj.children;
+  } else {
+    // sort links
+    leftObj.children.sort((a, b) => a.id - b.id);
+  }
+};
+
 router.route('/courses').get(async (req, res) => {
   try {
-    const list = [];
+    const list = { children: [] };
     const facs = await db.query('select * from Faculty');
 
-    /* eslint-disable no-restricted-syntax, no-await-in-loop */
     for (const fac of facs) {
-      const facObj = {
-        id: fac.FacultyId,
-        title: fac.Description,
-        links: [], // Faculty courses
-        children: [], // Department
-      };
-      list.push(facObj); // linking
-
-      const depts = await db.query(`
-        select * from Department
-          where FacultyId = "${fac.FacultyId}"
-      `);
-      for (const dept of depts) {
-        const deptObj = {
-          id: dept.DepartmentId,
-          title: dept.Description,
-          links: [], // Department courses
-        };
-        facObj.children.push(deptObj); // linking
-
-        const courses = await db.query(`
-          select * from Course
-            where DepartmentId = "${dept.DepartmentId}"
-        `);
-
-        for (const course of courses) {
-          // linking
-          deptObj.links.push({
-            id: course.CourseId,
-            title: course.CourseId.toUpperCase(),
-            description: course.Description,
-          });
-        }
-        // unset empty links
-        if (deptObj.links.length === 0) {
-          delete deptObj.links;
-        } else {
-          // sort links
-          deptObj.links.sort((a, b) => a.id - b.id);
-        }
-      }
-
-      const courses = await db.query(`
-        select * from Course
-          where FacultyId = "${fac.FacultyId}"
-      `);
-      for (const course of courses) {
-        // linking
-        facObj.links.push({
-          id: course.CourseId,
-          title: course.CourseId.toUpperCase(),
-          description: course.Description,
-        });
-      }
-      // unset empty links
-      if (facObj.links.length === 0) {
-        delete facObj.links;
-      } else {
-        // sort links
-        facObj.links.sort((a, b) => a.id - b.id);
-      }
-      // unset empty children
-      if (facObj.children.length === 0) {
-        delete facObj.children;
-      } else {
-        // sort links
-        facObj.children.sort((a, b) => a.id - b.id);
-      }
+      await deepLinkToRight(fac, list, {
+        idName: 'FacultyId',
+        childrenQuery: `
+          select * from Department
+            where FacultyId = "${fac.FacultyId}"
+            and SchoolId is null
+        `,
+      });
     }
-    /* eslint-disable no-restricted-syntax, no-await-in-loop */
-    res.json(list);
+    res.json(list.children);
   } catch (err) {
     console.log(err);
     res.send(err.message);
   }
 });
+/* eslint-disable no-restricted-syntax, no-await-in-loop */
 
 module.exports = router;
