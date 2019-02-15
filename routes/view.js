877892ea028f675739023632f5701047597c625d
chiuchinyan <33278516+chiuchinyan@users.noreply.github.com>
2019-02-16 01:42:02 +0800

    Features/view post (#30)
    * changing hash
    
    * change to 'subtitle'
    
    * view native post
    
    * wip
    
    * create view.js
    
    * Edit openapi
    
    * visitPost to extract all discussions in post
    
    * add example for using crawler.visitPost
    
    * wip
    
    * view post from moodle done
    
    * delete mvp.dev.js

---
+++ routes/view.js
@@ -0,0 +1,97 @@
+const express = require('express');
+
+const crawler = require('../moodle/crawler');
+const { db } = require('../database/connect');
+const { decrypt } = require('../security/safe');
+const { responseError, responseSuccess } = require('./helper');
+const { tokenGatekeeper } = require('./auth');
+
+const router = express.Router();
+
+const getNativeReply = async (topicId) => {
+  try {
+    // get native post from database
+    const topic = await db.query({
+      sql: `select P.*
+              from Topic T
+              join Post as P on T.PostId = P.PostId
+              where T.TopicId = ?`,
+      values: [topicId],
+    });
+    const resTopic = topic[0];
+    const resultPosts = [{
+      id: resTopic.PostId,
+      timestamp: resTopic.Timestamp,
+      temperature: resTopic.Temperature,
+      content: resTopic.Content,
+    }];
+    if (!resTopic.Anonymous) resultPosts[0].author = resTopic.Author;
+    // get native replies
+    const reply = await db.query({
+      sql: `select P.*
+              from Topic T
+              left join Reply as R on T.TopicId = R.TopicId
+              join Post as P on R.PostId = P.PostId
+              where T.TopicId = ?
+              order by P.Timestamp asc`,
+      values: [topicId],
+    });
+    for (let i = 0; i < reply.length; i += 1) {
+      const dbobj = reply[i];
+      const resultobj = {
+        id: dbobj.PostId,
+        timestamp: dbobj.Timestamp,
+        temperature: dbobj.Temperature,
+        content: dbobj.Content,
+      };
+      if (!dbobj.Anonymous) resultobj.author = dbobj.Author;
+      resultPosts.push(resultobj);
+    }
+    return resultPosts;
+  } catch (e) {
+    throw new Error('database-error');
+  }
+};
+
+router.route('/:topicId').post(async (req, res) => {
+  const { topicId } = req.params;
+  const { username, token, moodleKey } = req.body;
+
+  try {
+    // check username and token are matched
+    await tokenGatekeeper({ userId: username, token });
+    // check moodleKey is valid
+    const cookieString = decrypt(moodleKey);
+    const isLoggedIn = await crawler.proveLogin({
+      cookieString,
+    });
+    if (isLoggedIn) {
+      if (topicId.startsWith('mod')) {
+        const result = await crawler.visitPost({ cookieString, postId: topicId });
+        responseSuccess(result, res, 200);
+      } else {
+        const result = await getNativeReply(topicId);
+        responseSuccess(result, res);
+      }
+    } else {
+      responseError(408, res);
+    }
+  } catch (err) {
+    console.log(err);
+    switch (err.message) {
+      case 'moodle-not-enrolled':
+        responseError(412, res);
+        break;
+      case 'database-error':
+        responseError(502, res);
+        break;
+      case 'login-error':
+        responseError(401, res);
+        break;
+      default:
+        responseError(500, res);
+    }
+  }
+});
+
+module.exports = router;
