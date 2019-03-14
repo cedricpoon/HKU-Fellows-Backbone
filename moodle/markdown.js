baae05b9cf302756a564784dcd53ea1798cedd3b
Cedric Poon <cedric@poons.me>
2019-03-15 01:18:20 +0800

    md to html in moodle compose

--- moodle/markdown.js
+++ moodle/markdown.js
@@ -1,7 +1,10 @@
 const TurndownService = require('turndown');
+const Showdown = require('showdown');
 
 const turndown = new TurndownService();
 
+const showdown = new Showdown.Converter();
+
 turndown.remove('script');
 
-module.exports = { turndown };
+module.exports = { turndown, showdown };
